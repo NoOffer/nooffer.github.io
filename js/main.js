b23e7229f9aa72da6378d1d276a1bdec3991fa7c
Nofer <nortusx@gmail.com>
2025-06-10 00:21:49 +0800

    Finished new version

--- js/main.js
+++ js/main.js
@@ -1,33 +1,53 @@
-// Get all pages
 const pages = document.querySelectorAll('.page');
+const navItems = document.querySelectorAll('.nav_item');
+console.log(`Total page: ${pages.length}`);
 
+// On load
+pages.forEach((page, i) => {
+  page.style.transform = `translateY(${i * 100}vh)`;
+});
+setTimeout(() => {
+  pages.forEach((page, _) => {
+    page.style.transition = 'transform 0.5s ease';
+  });
+}, 20);
+
+navItems.forEach((item, i) => {
+  item.addEventListener('click', () => { scrollToPage(i); });
+});
+
+// State
 let currPageIdx = 0;
-
-// Prevent multiple scrolls
 let isScrolling = false;
 
-function onScroll(index) {
+// Behavior
+function scrollToPage(index) {
+  console.log(`Currently at page ${currPageIdx}, scrolling to page ${index}`);
   if (index < 0 || index >= pages.length || isScrolling) return;
-
+  
   isScrolling = true;
 
   pages.forEach((page, i) => {
     // Move each page up or down depending on its position
     page.style.transform = `translateY(${(i - index) * 100}vh)`;
   });
+  
+  navItems.forEach((item, i) => {
+    item.classList.toggle('active', i === index);
+  });
 
   // Allow next scroll
   setTimeout(() => {
     currPageIdx = index;
     isScrolling = false;
-  }, 700);
+  }, 300);
 }
 
-// Listen for mouse scrolls
+// Event callback
 document.addEventListener('wheel', (event) => {
   if (event.deltaY > 0) {
-    onScroll(currPageIdx + 1);
+    scrollToPage(currPageIdx + 1);
   } else {
-    onScroll(currPageIdx - 1);
+    scrollToPage(currPageIdx - 1);
   }
 });
